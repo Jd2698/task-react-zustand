2936103c6a8e7bd3e988aae4a3900ac8c5e8f066
Jd2698 <j.delgado2698@gmail.com>
2025-06-16 20:30:07 -0500

    adjust table responsive

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -23,7 +23,7 @@ export const ProductsList = () => {
 
 	return (
 		<>
-			<div className="lg:col-span-2 overflow-x-auto">
+			<div className="lg:col-span-2">
 				<div className="text-end space-x-1 mb-2">
 					<input
 						type="text"
@@ -44,53 +44,55 @@ export const ProductsList = () => {
 					</select>
 				</div>
 
-				<table className="w-full text-left table-auto rounded-lg shadow-lg bg-[#1a1a1a]">
-					<thead>
-						<tr className="text-white bg-[#212121] border-b border-gray-700">
-							<th className="px-6 py-4 text-sm font-semibold text-gray-400">
-								Description
-							</th>
-							<th className="px-6 py-4 text-sm font-semibold text-gray-400">
-								Status
-							</th>
-							<th className="px-6 py-4 text-sm text-center font-semibold text-gray-400">
-								Actions
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						{data.map(p => (
-							<tr key={p.id} className="text-gray-300 hover:bg-[#242424]">
-								<td className="px-6 py-4 border-b border-gray-700">
-									{p.description}
-								</td>
-								<td className="px-6 py-4 border-b border-gray-700">
-									<span
-										className={`font-semibold ${
-											p.completed ? 'text-green-400' : 'text-red-400'
-										}`}
-									>
-										{p.completed ? 'Completed' : 'Pending'}
-									</span>
-								</td>
-								<td className="flex flex-wrap gap-2 justify-center px-6 py-4 border-b border-gray-700">
-									<button
-										onClick={() => setSelectedProduct(p.id)}
-										className="bg-green-600/80 text-white px-4 py-2 rounded-md text-sm hover:bg-green-600/50 transition duration-300"
-									>
-										Edit
-									</button>
-									<button
-										onClick={() => removeProduct(p.id)}
-										className="bg-red-600/80 text-white px-4 py-2 rounded-md text-sm hover:bg-red-600/50 transition duration-300"
-									>
-										Delete
-									</button>
-								</td>
+				<div className="overflow-x-auto">
+					<table className="w-full text-left table-auto rounded-lg shadow-lg bg-[#1a1a1a]">
+						<thead>
+							<tr className="text-white bg-[#212121] border-b border-gray-700">
+								<th className="px-6 py-4 text-sm font-semibold text-gray-400">
+									Description
+								</th>
+								<th className="px-6 py-4 text-sm font-semibold text-gray-400">
+									Status
+								</th>
+								<th className="px-6 py-4 text-sm text-center font-semibold text-gray-400">
+									Actions
+								</th>
 							</tr>
-						))}
-					</tbody>
-				</table>
+						</thead>
+						<tbody>
+							{data.map(p => (
+								<tr key={p.id} className="text-gray-300 hover:bg-[#242424]">
+									<td className="px-6 py-4 border-b border-gray-700">
+										{p.description}
+									</td>
+									<td className="px-6 py-4 border-b border-gray-700">
+										<span
+											className={`font-semibold ${
+												p.completed ? 'text-green-400' : 'text-red-400'
+											}`}
+										>
+											{p.completed ? 'Completed' : 'Pending'}
+										</span>
+									</td>
+									<td className="flex flex-wrap gap-2 justify-center px-6 py-4 border-b border-gray-700">
+										<button
+											onClick={() => setSelectedProduct(p.id)}
+											className="bg-green-600/80 text-white px-4 py-2 rounded-md text-sm hover:bg-green-600/50 transition duration-300"
+										>
+											Edit
+										</button>
+										<button
+											onClick={() => removeProduct(p.id)}
+											className="bg-red-600/80 text-white px-4 py-2 rounded-md text-sm hover:bg-red-600/50 transition duration-300"
+										>
+											Delete
+										</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
 				<div className="flex justify-evenly py-2">
 					<button onClick={() => prevPage()}>previous</button>
 					<span>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { ProductsList } from '../components/ProductsList'
 function Home() {
 	return (
 		<>
-			<main className="max-w-7xl m-auto my-10 pt-10 px-20 grid auto-rows-auto grid-cols-1 lg:grid-cols-3 gap-4">
+			<main className="max-w-7xl m-auto my-10 pt-10 px-5 sm:px-20 grid auto-rows-auto grid-cols-1 lg:grid-cols-3 gap-4">
 				<Form />
 				<ProductsList />
 			</main>
